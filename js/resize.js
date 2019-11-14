036cef052dd555ba44b9be2d4bcdfb921260c5b7
Karol Chojnacki <karol.chojnacki@icloud.com>
2019-11-14 14:59:56 +0100

    ukrywanie i pokazywanie menu

--- js/resize.js
+++ js/resize.js
@@ -1,8 +1,10 @@
-// $(function() {
-//   $('#photo').hover(function() {
-//     $('#hallprop').css('height', '420');
-//   }, function() {
-//     // on mouseout, reset the background colour
-//     $('#hallprop').css('height', '');
-//   });
-// });
+$(".mobilebar").on('click', function() {
+  $(".button.mobile").css("visibility", "hidden");
+  $(".sidebar").css("visibility", "visible").css("width", "100px");
+  $(".button.bmenu").css("color", "black").css("width", "100px");
+    setTimeout(function(){
+      $(".button.mobile").css("visibility", "visible");
+      $(".sidebar").css("visibility", "hidden");
+      $(".button.bmenu").css("width", '');
+    }, 5000);
+});
