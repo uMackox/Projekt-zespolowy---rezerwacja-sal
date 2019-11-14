39fb8e5baadc30fe9185792797c5f0fb9744b448
Karol Chojnacki <karol.chojnacki@icloud.com>
2019-11-14 15:07:46 +0100

    ukrywanie menu podczas przewijania strony

--- js/resize.js
+++ js/resize.js
@@ -2,9 +2,9 @@ $(".mobilebar").on('click', function() {
   $(".button.mobile").css("visibility", "hidden");
   $(".sidebar").css("visibility", "visible").css("width", "100px");
   $(".button.bmenu").css("color", "black").css("width", "100px");
-    setTimeout(function(){
+    $(window).scroll(function() {
       $(".button.mobile").css("visibility", "visible");
       $(".sidebar").css("visibility", "hidden");
       $(".button.bmenu").css("width", '');
-    }, 5000);
+    });
 });
