60f4edc9e5a4254fd68744d89c9e9b7c160f645f
Karol Chojnacki <karol.chojnacki@icloud.com>
2019-11-14 11:05:20 +0100

    przeglądanie sal - template

---
+++ js/resize.js
@@ -0,0 +1,8 @@
+// $(function() {
+//   $('#photo').hover(function() {
+//     $('#hallprop').css('height', '420');
+//   }, function() {
+//     // on mouseout, reset the background colour
+//     $('#hallprop').css('height', '');
+//   });
+// });
